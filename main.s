bb3249579dfe975ffad1e04ce19fac2294489f82
Nyanserad <oskar.hogberg@gmail.com>
2021-02-19 11:54:58 +0000

    Delay using a 16-bit number

--- main.s
+++ main.s
@@ -7,50 +7,42 @@ main:
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
-errorMessage:
-	movlw	0xAA
-	movwf	PORTC, A
-	return	0
-start:
-;	movlw 	0x55
-;	movwf	0x50, A	    
-;	movlw	0x00
-;	movwf	0x10, A
-    
-	c0	equ 0x10
-	c1	equ 0x20
-	init	equ 0x330
-	len	equ 0x03
 	
+largeDelay:
 	movlw	0x00
-	movwf	c0, A
-	movwf	c1, A
-	movwf	TRISC, A
+ldLoop:	; There is only no carry when going from 0 -> 0xFF for both hi and low
+	decf	lo, f, A
+	subwfb	hi, f, A
+	bc	ldLoop	    ; loop if carry
+	return 0	    ; return when no carry
+
+start:
+	c0	equ 0x10    ; counter location
+	len	equ 0xFFFF    ; lenght of delay
+	hi	equ 0x20    ; high 16 bit
+	lo	equ 0x21    ; low 16 bit
+	outLim	equ 0x7F    ; break counting loop at this value
 	
-	lfsr	0, init
-	lfsr	1, init
-writeLoop:
-	movf	c0, W, A
-;	movlw	0x00
-	movwf	POSTINC0, A
-	incf	c0, A
-	movlw	len
+	movlw	0x00	    ; initialise counter to 0
+	movwf	c0, A	    
+	movwf	TRISC, A    ; set c to output mode
+	
+
+count:
+	movlw	high(len)   ; reset delay value
+	movwf	hi, A
+	movlw	low(len)
+	movwf	lo, A
+	    
+	call	largeDelay  ; call delay
+	
+	movf	c0, W, A    ; put counter value on port c
+	movwf	PORTC, A    
+	incf	c0, F, A    ; increment counter
+	movlw	outLim	    ; break if counter larger than break value
 	cpfsgt	c0, A
-	bra	writeLoop
-	
-readLoop:
-	movf	POSTINC1, W, A
-	cpfseq	c1, A
-	call	errorMessage
-	incf	c1, A
-	movlw	len
-	cpfsgt	c1, A
-	bra	readLoop
+	bra	count
+
+	bra	start
 	
 	
-	
-;loop:
-;	movf	0x10, W, A
-;	movwf	0x20, A
-;	incf	0x10, A
-;	bra	loop
