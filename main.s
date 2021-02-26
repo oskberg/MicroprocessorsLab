14381cc2c19a9450942438eec1e5ff54a6379f67
Nyanserad <oskar.hogberg@gmail.com>
2021-02-26 17:33:01 +0000

    testing if commit solves error when calling routine

--- main.s
+++ main.s
@@ -1,22 +1,15 @@
 #include <xc.inc>
 
-extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message, LCD_Clear_Screen, LCD_delay_ms, LCD_Cursor_Line_2, LCD_Write_Message_PM
-	
+extrn	readRow, readCol
+extrn	testRoutine
+    
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
-    
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+rowVal:	    ds 1    ; reserve one byte for the row value
+colVal:	    ds 1    ; reserve one byte for the column value
 
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
     
 psect	code, abs	
 rst: 	org 0x0
@@ -25,8 +18,14 @@ rst: 	org 0x0
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup UART
+		;setup keypad
+	call testRoutine
+	banksel PADCFG1
+	bsf	REPU	    ; set pull-ups
+	movlb	0
+	clrf	LATE, A	    ; write 0s to lat register
+	
 	goto	start
 	
 	; setup buttons on F for input
@@ -34,23 +33,7 @@ setup:	bcf	CFGS	; point to Flash program memory
 
 	
 	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
+start: 	
 
 writeLoop:
     	
@@ -63,29 +46,20 @@ writeLoop:
 	movlw	0xFE
 	call	LCD_delay_ms
 	
-;	; write to LCD from ram
+	; write row input to lcd
+	call	readRow		    ; move row reading into w
+	movwf	rowVal, A	    ; move row reading int rowVal
+	lfsr	2, rowVal
+	movlw	1
+	
+	call	LCD_Write_Message   ; write message
+	
+	
+;		; write to LCD from ram
 ;	movlw	myTable_l	; output message to LCD
 ;	addlw	0xff		; don't send the final carriage return to LCD
 ;	lfsr	2, myArray
 ;	call	LCD_Write_Message   ; write message
-	
-	; read buttons on e
-	movlw	0x00
-	cpfseq	PORTD, A
-	call	LCD_Cursor_Line_2   ; write on second line
-	
-	; LOAD TABLE POINTERS ;
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	
-	;write to LCD from tablat
-	movlw	myTable_l	; output message to LCD
-	addlw	0xff		; don't send the final carriage return to LCD
-	call	LCD_Write_Message_PM   ; write message
 
 	; delay
 	movlw	0xFE
