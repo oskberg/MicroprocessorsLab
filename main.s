ae286430d12164c25dfddcf09088fc0486390c01
Nyanserad <oskar.hogberg@gmail.com>
2021-03-01 10:25:11 +0000

    reading working i think

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
-extrn	LCD_Setup, LCD_Write_Message, LCD_Clear_Screen, LCD_delay_ms, LCD_Cursor_Line_2, LCD_Write_Message_PM
-extrn	readRow, readCol
+extrn	LCD_Setup, LCD_Write_Message, LCD_Clear_Screen, LCD_delay_ms, LCD_Cursor_Line_2, LCD_Write_Message_PM, LCD_delay_x4us
+;extrn	readRow, readCol
 extrn	testRoutine
     
 psect	udata_acs   ; reserve data space in access ram
@@ -9,6 +9,7 @@ counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
 rowVal:	    ds 1    ; reserve one byte for the row value
 colVal:	    ds 1    ; reserve one byte for the column value
+padVal:	    ds 1    ; reserve one byte for the pad output value
 
     
 psect	code, abs	
@@ -20,7 +21,7 @@ setup:	bcf	CFGS	; point to Flash program memory
 	bsf	EEPGD 	; access Flash program memory
 	call	LCD_Setup	; setup UART
 		;setup keypad
-	call	testRoutine
+;	call	testRoutine
 	banksel PADCFG1
 	bsf	REPU	    ; set pull-ups
 	movlb	0
@@ -39,21 +40,30 @@ writeLoop:
     	
 	; clear LCD
 	call	LCD_Clear_Screen
-
-	; delay
-	movlw	0xFE
-	call	LCD_delay_ms
-	movlw	0xFE
-	call	LCD_delay_ms
 	
 	; write row input to lcd
 	call	readRow		    ; move row reading into w
 	movwf	rowVal, A	    ; move row reading int rowVal
+	
+	call	readCol		    ; move col reading into w
+	movwf	colVal, A	    ; move col reading int rowVal
+	
+	iorwf	rowVal, W, A
+	movwf	padVal, A
+	
+	
+	
+	
+	
 	lfsr	2, rowVal
 	movlw	1
-	
 	call	LCD_Write_Message   ; write message
-	
+	lfsr	2, colVal
+	movlw	1
+	call	LCD_Write_Message   ; write message
+	lfsr	2, padVal
+	movlw	1
+	call	LCD_Write_Message   ; write message
 	
 ;		; write to LCD from ram
 ;	movlw	myTable_l	; output message to LCD
@@ -62,9 +72,7 @@ writeLoop:
 ;	call	LCD_Write_Message   ; write message
 
 	; delay
-	movlw	0xFE
-	call	LCD_delay_ms
-	movlw	0xFE
+	movlw	100
 	call	LCD_delay_ms
 	
 	bra	writeLoop
@@ -74,5 +82,23 @@ writeLoop:
 delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
 	return
-
+	
+readRow:	; read row output
+	movlw	0x0f	    ; set 0-3 as input and 4-7 as output on PORT E
+	movwf	TRISE, A
+	movlw	0x10	    ; 40 us delay to let pin volatges settle
+	call	LCD_delay_x4us ; delay
+	
+	movf	PORTE, W, A ; move result into w
+	return
+	
+readCol:
+	movlw	0xf0	    ; set 0-3 as OUTPUT and 4-7 as INPUT on PORT E
+	movwf	TRISE, A    
+	movlw	0x10	    ; 40 us delay to let pin volatges settle
+	call	LCD_delay_x4us ; delay
+	
+	movf	PORTE, W, A ; move result into w
+	return
+	
 	end	rst
